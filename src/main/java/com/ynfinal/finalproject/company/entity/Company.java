9113e15cbe83aa2af30389cf3e8c29e425535344
golddrone7 <golddrone@naver.com>
2023-06-14 23:57:35 +0900

    Company 엔터티 설계

--- src/main/java/com/ynfinal/finalproject/company/entity/Company.java
+++ src/main/java/com/ynfinal/finalproject/company/entity/Company.java
@@ -1,6 +1,28 @@
 package com.ynfinal.finalproject.company.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "my_company")
 public class Company {
+
+    @Id
+    String compCode;
+    String compName;
+    String compRegNo;
+    String compCeo;
+    String compPhone;
 
 
 }
