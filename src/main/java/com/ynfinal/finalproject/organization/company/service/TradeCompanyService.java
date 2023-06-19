89f88672102abf4988178c545d2967dc0253922e
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-06-19 09:47:06 +0900

    api 폴더 구조

---
+++ src/main/java/com/ynfinal/finalproject/organization/company/service/TradeCompanyService.java
@@ -0,0 +1,14 @@
+package com.ynfinal.finalproject.organization.company.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+public class TradeCompanyService {
+}
---
+++ src/main/java/com/ynfinal/finalproject/organization/user/service/AuthorizationService.java
@@ -0,0 +1,15 @@
+package com.ynfinal.finalproject.organization.user.service;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+public class AuthorizationService {
+}
