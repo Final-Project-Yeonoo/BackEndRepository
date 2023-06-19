adacfbecd9dea5a7854d7145461af7b0a1aa3e50
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-06-19 21:29:35 +0900

    회원가입 DTO 수정

--- src/main/java/com/ynfinal/finalproject/organization/user/dto/request/EmployeesSignUpRequestDto.java
+++ src/main/java/com/ynfinal/finalproject/organization/user/dto/request/EmployeesSignUpRequestDto.java
@@ -1,17 +1,20 @@
 package com.ynfinal.finalproject.organization.user.dto.request;
 
 
+import com.ynfinal.finalproject.organization.company.entity.Company;
 import com.ynfinal.finalproject.organization.user.entity.Authorization;
 import com.ynfinal.finalproject.organization.user.entity.Department;
 import com.ynfinal.finalproject.organization.user.entity.Employees;
 import com.ynfinal.finalproject.organization.user.entity.Position;
 import com.ynfinal.finalproject.util.Check;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 
@@ -33,21 +36,27 @@ public class EmployeesSignUpRequestDto {
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,30}$",
             message = "비밀번호는 8~30 자리이면서 1개 이상의 알파벳, 숫자, 특수문자를 포함해야합니다.")
     private String empPassword;
+    @NotNull(message = "부서코드를 입력해주세요")
     private Long deptCode;
+    @NotNull(message = "직급코드를 입력해주세요")
     private Long posCode;
+    @NotBlank(message = "휴대폰번호를 입력해주세요")
     private String empPhone;
+    @NotBlank(message = "내선번호를 입력해주세요")
     private String empExtension;
+    @NotNull(message = "입사일을 입력해주세요")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime empHiredDate;
-    private boolean empValidate;
+    private boolean empValidate = false;
 
     @Enumerated(EnumType.STRING)
-    private Check userAuth;
+    private Check userAuth = Check.N;
     @Enumerated(EnumType.STRING)
-    private Check infoAuth;
+    private Check infoAuth= Check.N;
     @Enumerated(EnumType.STRING)
-    private Check purchaseAuth;
+    private Check purchaseAuth= Check.N;
     @Enumerated(EnumType.STRING)
-    private Check inventoryAuth;
+    private Check inventoryAuth= Check.N;
 
 
     public Employees toEntity(){
@@ -61,6 +70,8 @@ public class EmployeesSignUpRequestDto {
                 .empExtension(empExtension)
                 .empHiredDate(empHiredDate)
                 .empValidate(empValidate)
+                //TODO 사원 관리를 등록하는 사람의 회사 코드를 가져오기
+                .company(Company.builder().compCode(1L).build())
                 .build();
     }
 
