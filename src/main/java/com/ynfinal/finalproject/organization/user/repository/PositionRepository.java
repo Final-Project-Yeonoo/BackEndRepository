0cb24628b7baaad7dba2dd6d6dab78668730ddc2
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-06-28 13:49:57 +0900

    controller + service

--- src/main/java/com/ynfinal/finalproject/organization/user/repository/PositionRepository.java
+++ src/main/java/com/ynfinal/finalproject/organization/user/repository/PositionRepository.java
@@ -2,7 +2,7 @@ package com.ynfinal.finalproject.organization.user.repository;
 
 import com.ynfinal.finalproject.organization.user.entity.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//
 public interface PositionRepository
         extends JpaRepository<Position, Long> {
 
